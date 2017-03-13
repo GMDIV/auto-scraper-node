b40761b5b02a8bc5d4951a6e61a94434188d5cc9
Mitchell Deane <mitchelld20@yahoo.com>
2017-03-13 13:56:51 -0500

    Added cherrio for better element selection. Parsed down the make and most of the inventory count

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var request = require('request');
+var cheerio = require('cheerio');
 
 var url = "https://www.netdirectautosales.com/inventory/";
 
@@ -9,7 +10,21 @@ request(url, function(error, response, html) {
     //console.log("html:", html); // Print the HTML for the Google homepage.
     console.log('error:', error); // Print the error if one occurred 
  	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-      
+    if(!error && response.statusCode == 200){
+    	var $ = cheerio.load(html);
+    	$('.filter-option-make').each(function(i,element){
+    		var el = $(this);
+    		//console.log(el)
+    		//console.log(element)
+    		var makeData = {
+    			name: element.attribs['data-option'],
+    			count: element.children[2].prev.children
+
+    		}
+    		console.log(makeData.name)
+    		console.log(makeData.count)
+    	})
+    }  
 });
 
 
